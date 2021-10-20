6f0cf00b9d006abc71d838bc61429800b1fe80c1
SvitlanaZhovanik <Lanasvetcat@gmail.com>
2021-10-20 21:27:02 +0300

    Реализованы функции смены цвета

--- src/index.js
+++ src/index.js
@@ -4,8 +4,34 @@ import colors from './color.js'
 
 const btnStartRef = document.querySelector('#start');
 const btnStopRef = document.querySelector('#stop');
-
+const bodyRef = document.querySelector('body');
+let isActive = false;
+let switchId = null;
 
 const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
+
+
+  const onBtnStartClick = function (){
+   if (isActive) {
+     return
+   }
+  switchId = setInterval(()=>{
+  isActive = true;
+  let idx = randomIntegerFromInterval(0, colors.length)
+  setBodyBgColor (colors[idx])
+}, 2000)
+  };
+  function setBodyBgColor(color) {
+    bodyRef.style.backgroundColor = color;
+  }
+
+
+  btnStartRef.addEventListener('click', onBtnStartClick)
+  btnStopRef.addEventListener('click', () => {
+    clearInterval(switchId);
+    isActive = false;
+  });
+
+  
